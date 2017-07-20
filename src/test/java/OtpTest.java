bf1baa605c46ee093185c8f1a2c55b13c1027a37
Akivamu <akivamu@gmail.com>
2017-07-20 18:46:07 +0700

    Fix: missing leading zeros

--- src/test/java/OtpTest.java
+++ src/test/java/OtpTest.java
@@ -12,5 +12,15 @@ public class OtpTest {
 
         Assert.assertEquals("557487", TOTP.generateToken("TQCEOJ5TVODDC2MT", time));
         Assert.assertEquals("695218", TOTP.generateToken("ufsv mhe5 ztrt fcpa hr7r fe3p wn4q bstc", time));
+
+        time = 1500551085162L;
+        Assert.assertEquals("067530", TOTP.generateToken("atbr esm5 igmg vjv2 ac7s deqi 5bq4 fyhs", time));
+
+        runNow();
+    }
+
+    private void runNow() {
+        long now = System.currentTimeMillis();
+        System.out.println(now + " - " + TOTP.generateToken("atbr esm5 igmg vjv2 ac7s deqi 5bq4 fyhs", now));
     }
 }
